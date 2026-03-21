a3966a67dfbb0a4728e00b2be6ca0bd3ed54b222
Raul <raulgeisler@icloud.com>
2026-03-21 18:15:07 +0100

    Initial Commit

---
+++ AppSwitcher/AppSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwitcherApp.swift
+//  AppSwitcher
+//
+//  Created by Raul Geisler on 21.03.26.
+//
+
+import SwiftUI
+
+@main
+struct AppSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppSwitcher
+//
+//  Created by Raul Geisler on 21.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
